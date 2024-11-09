7e0015615f7a2b65d39dbfa571676a1b2c147b55
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-11-10 00:16:36 +0530

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import About from "../components/About";
 import Services from "../components/Services";
 import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
-
+import Testimonials from "../components/Testimonials"
 
 export default function HomePage() {
     return (
@@ -16,7 +16,7 @@ export default function HomePage() {
             <About />
             <Services />
             <Portfolio />
-            
+            <Testimonials/>
             <Footer />
         </div>
     );
