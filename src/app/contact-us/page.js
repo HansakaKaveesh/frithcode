42a0286b61fc03765fb6532c090a38d3684e75ef
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-11-20 19:25:47 +0530

    edit

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -119,17 +119,7 @@ const ContactUs = () => {
           <strong>Address:</strong> 123 Web Development St, Tech City
         </li>
       </ul>
-      <h2 className="text-2xl font-semibold mb-4 text-gray-900 dark:text-white">
-        Find Us
-      </h2>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.835434509629!2d144.96315761531665!3d-37.81362797975168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d3c9da2d22a5!2sMelbourne!5e0!3m2!1sen!2sau!4v1638764516963!5m2!1sen!2sau"
-        width="100%"
-        height="300"
-        allowFullScreen=""
-        loading="lazy"
-        className="border rounded dark:border-gray-600"
-      ></iframe>
+      
     </div>
   </div>
 </div>
