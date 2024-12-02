aed5e0ce00033a2c61629af065a8b728bb8b2d77
HansakaKaveesh <hansakakaveesh2@gmail.com>
2024-12-02 19:05:18 +0530

    edit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -106,7 +106,7 @@ export default function Navigation() {
                                     Web Development</li>
                                 </Link>
 
-                                <Link href="/services/app-development"><li className="px-4 py-2 text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-800 cursor-pointer">
+                                <Link href="/services/e-commerce-solutions"><li className="px-4 py-2 text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-800 cursor-pointer">
                                     App Development</li>
                                 </Link>
 
