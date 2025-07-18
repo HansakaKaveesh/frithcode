8ef061f2ea5f401bf288927c4fa67e1a5d6e23f5
HansakaKaveesh <hansakakaveesh2@gmail.com>
2025-07-18 12:57:51 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/services/e-commerce-solutions/page.js
+++ src/app/services/e-commerce-solutions/page.js
@@ -3,12 +3,11 @@ import HeroSection from './components/HeroSection';
 import WhyChooseUs from './components/WhyChooseUs';
 import OurServices from './components/OurServices';
 import ContactCTA from './components/ContactCTA';
-import Navigation from "../../../components/Navigation";
 
 const ecommerce = () => {
   return (
     <div>
-      <Navigation/>
+   
     <div className="min-h-screen bg-gray-50 text-gray-800">
       
       <HeroSection />
--- src/app/services/web-development/page.js
+++ src/app/services/web-development/page.js
@@ -3,12 +3,12 @@ import HeroSection from './components/HeroSection';
 import WhyChooseUs from './components/WhyChooseUs';
 import OurServices from './components/OurServices';
 import ContactCTA from './components/ContactCTA';
-import Navigation from "../../../components/Navigation";
+
 
 const WebDevelopment = () => {
   return (
     <div>
-      <Navigation/>
+
     <div className="min-h-screen bg-gray-50 text-gray-800">
       
       <HeroSection />
